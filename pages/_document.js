77a9c0621fc6d0b7589cb357f1643302b5a1f282
aludayalu <pl46u3.ducklinz@gmail.com>
2024-01-17 19:13:44 +0530

    added meta

--- pages/_document.js
+++ pages/_document.js
@@ -16,6 +16,7 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>{CssBaseline.flush()}
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap" rel="stylesheet"/>
+        <meta name="viewport" content="width=device-width, initial-scale=0.6, maximum-scale=0.6,minimum-scale=0.6"/>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/1.3.8/FileSaver.js"></script>
         </Head>
         <body>
